9eb6e6461287ab0aa98ac11dc3fd562397234e07
fdlqiang <lq765952655@163.com>
2021-10-25 17:51:53 +0800

    完成了一些页面的实现

--- src/components/container/SaveNewTask.vue
+++ src/components/container/SaveNewTask.vue
@@ -5,8 +5,13 @@
                :visible.sync="dialogVisible"
                width="80%">
       <el-form :model="saveNewTaskData" ref="saveNewTaskRef" label-width="90px" size="medium" :rules="saveNewTaskRules">
+        <!-- 卡片标题 -->
+        <div class="title-box">
+          <div class="title">
+            基础信息
+          </div>
+        </div>
         <!-- 卡片区域 -->
-        <span>基础信息</span>
         <el-card class="box-card">
           <el-row>
             <el-col :span="12">
@@ -26,8 +31,7 @@
               <el-form-item label="牵头部门" prop="leaderDepartmentName">
                 <input type="hidden" v-model="saveNewTaskData.leaderDepartmentName">
                 <el-select v-model="saveNewTaskData.leaderDepartmentId" filterable
-                           placeholder="请选择" @change="getDepartmentNameByDepartmentCode"
-                >
+                           placeholder="请选择" @change="getDepartmentNameByDepartmentCode">
                   <el-option
                     v-for="(item,index) in leaderDepartmentOptions"
                     :key="index"
@@ -46,7 +50,7 @@
                   <el-option
                     v-for="(item,index) in leaderUserOptions"
                     :key="index"
-                    :label="item.userName"
+                    :label="item.userName +'【'+ item.departmentName + '】'"
                     :value="item.userCode">
                   </el-option>
                 </el-select>
@@ -87,7 +91,8 @@
                   type="datetime"
                   placeholder="选择任务开始日期时间"
                   default-time="12:00:00"
-                  value-format="yyyy-MM-dd HH:mm:ss">
+                  value-format="yyyy-MM-dd HH:mm:ss"
+                  :picker-options="pickerOptionsStart">
                 </el-date-picker>
               </el-form-item>
             </el-col>
@@ -98,7 +103,8 @@
                   type="datetime"
                   placeholder="选择任务截至日期时间"
                   default-time="12:00:00"
-                  value-format="yyyy-MM-dd HH:mm:ss">
+                  value-format="yyyy-MM-dd HH:mm:ss"
+                  :picker-options="pickerOptionsEnd">
                 </el-date-picker>
               </el-form-item>
             </el-col>
@@ -110,6 +116,70 @@
               </el-form-item>
             </el-col>
           </el-row>
+        </el-card>
+        <!-- 卡片标题 -->
+        <div class="title-box">
+          <div class="title">
+            人员任务
+          </div>
+        </div>
+        <!-- 人员任务卡片区域 -->
+        <el-card class="box-card">
+          <el-row v-for="item in saveNewTaskData.taskPersonDTOList" :key="item.userCode">
+            <el-col :span="6">
+              <el-form-item
+                label="参与人" prop="taskPerson">
+                <input type="hidden" v-model="item.userName">
+                <el-select v-model="item.userId" filterable
+                           placeholder="请选择" @change="getUserNameByUserCode">
+                  <el-option
+                    v-for="(item,index) in leaderUserOptions"
+                    :key="index"
+                    :label="item.userName +'【'+ item.departmentName + '】'"
+                    :value="item.userCode">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item
+                label="任务描述">
+                <el-input v-model="item.taskDescribe"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="6">
+              <el-form-item
+                label="分配工时">
+                <el-input-number v-model="item.workingHours" :precision="1" :step="0.1" :min="0.2"
+                                 :max="10"></el-input-number>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-form-item>
+            <el-button type="primary" @click="addDomain">增加参与人</el-button>
+          </el-form-item>
+        </el-card>
+        <!-- 卡片标题 -->
+        <div class="title-box">
+          <div class="title">
+            任务附件
+          </div>
+        </div>
+        <!-- 任务附件卡片区域 -->
+        <el-card class="box-card">
+          <el-upload
+            name="file"
+            action="http://localhost:8898/file/uploadFile"
+            :on-success="uploadSuccess"
+            :on-remove="handleRemove"
+            :before-remove="beforeRemove"
+            multiple
+            :limit="10"
+            :on-exceed="handleExceed"
+            :file-list="fileList">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">上传文件大小不超过3M</div>
+          </el-upload>
         </el-card>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -128,13 +198,17 @@ export default {
   name: 'SaveNewTask',
   data () {
     return {
+      dialogVisible: false,
+      // 上传的文件列表
+      fileList: [],
+      // 任务列表请求参数
       queryInfo: {
         pageNum: 1,
         pageSize: 10,
         taskName: '',
         publisherId: ''
       },
-      dialogVisible: false,
+      // 添加任务列表参数
       saveNewTaskData: {
         taskName: '',
         workingHours: 1.0,
@@ -146,8 +220,18 @@ export default {
         taskType: 1,
         startTime: '',
         endTime: '',
-        taskDetail: ''
+        taskDetail: '',
+        taskPersonDTOList: [
+          {
+            userId: '',
+            userName: '',
+            taskDescribe: '',
+            workingHours: ''
+          }
+        ],
+        taskAttachmentDTOList: []
       },
+      // 表单验证
       saveNewTaskRules: {
         taskName: [
           {
@@ -163,13 +247,15 @@ export default {
             trigger: 'blur'
           }
         ],
+        // TODO 牵头部门必填验证失效
         leaderDepartmentName: [
           {
             required: true,
             message: '请输入牵头部门',
-            trigger: 'blur'
+            trigger: ['blur', 'change']
           }
         ],
+        // TODO 牵头人必填验证失效
         leaderUserName: [
           {
             required: true,
@@ -211,6 +297,22 @@ export default {
             message: '请输入任务详情',
             trigger: 'blur'
           }
+        ],
+        // TODO 参与人必填验证失效
+        taskPerson: [
+          {
+            required: true,
+            message: '请选择参与人',
+            trigger: 'blur'
+          }
+        ],
+        // TODO 验证失效
+        taskDescribe: [
+          {
+            required: true,
+            message: '请输入任务描述',
+            trigger: 'blur'
+          }
         ]
       },
       dynamicValidateForm: {
@@ -242,7 +344,24 @@ export default {
       }],
       leaderUserOptions: {},
       leaderDepartmentOptions: {},
-      value: ''
+      value: '',
+      // 限制开始日期和结束日期的
+      pickerOptionsStart: {
+        disabledDate: time => {
+          if (this.saveNewTaskData.endTime
+          ) {
+            return time.getTime() > new Date(this.saveNewTaskData.endTime
+            ).getTime()
+          }
+        }
+      },
+      pickerOptionsEnd: {
+        disabledDate: time => {
+          if (this.saveNewTaskData.startTime) {
+            return time.getTime() < new Date(this.saveNewTaskData.startTime).getTime() - 86400000
+          }
+        }
+      }
     }
   },
   created () {
@@ -255,11 +374,32 @@ export default {
     eventBus.$on('leaderDepartmentOptions', (val) => {
       this.leaderDepartmentOptions = val
     })
-    console.log(this.leaderUserOptions)
-    console.log(this.leaderDepartmentOptions)
   },
 
   methods: {
+    uploadSuccess (response) {
+      // 上传成功之后需要把上传成功的id，集合到一个数组中
+      this.saveNewTaskData.taskAttachmentDTOList.push({
+        uploadFileId: response.data.uploadFileId
+      })
+    },
+    async handleRemove (file) {
+      // 调用删除接口，删除数据库数据
+      const { data: res } = await this.$http.delete('/file/removeUploadFile', {
+        params: {
+          id: file.response.data.uploadFileId
+        }
+      })
+      if (res.code !== 200) {
+        return this.$message.error('附件删除失败')
+      }
+    },
+    handleExceed (files, fileList) {
+      this.$message.warning(`当前限制选择 10 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
+    },
+    beforeRemove (file) {
+      return this.$messageBox.confirm(`确定移除 ${file.name}？`)
+    },
     getDepartmentNameByDepartmentCode (val) {
       this.leaderDepartmentOptions.forEach((item) => {
         if (val === item.departmentCode) {
@@ -271,6 +411,9 @@ export default {
       this.leaderUserOptions.forEach((item) => {
         if (val === item.userCode) {
           this.saveNewTaskData.leaderUserName = item.userName
+          this.saveNewTaskData.taskPersonDTOList.forEach((items, index) => {
+            this.saveNewTaskData.taskPersonDTOList[index].userName = item.userName
+          })
         }
       })
     },
@@ -307,9 +450,11 @@ export default {
       }
     },
     addDomain () {
-      this.dynamicValidateForm.domains.push({
-        value: '',
-        key: Date.now()
+      this.saveNewTaskData.taskPersonDTOList.push({
+        userCode: '',
+        userName: '',
+        taskDescribe: '',
+        workingHours: ''
       })
     }
   }
@@ -317,5 +462,24 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.title-box {
+  display: flex;
+  height: 40px;
+  margin-bottom: 15px;
+  border-bottom: solid 4px #00CCCC;
 
+  .title {
+    color: white;
+    background-color: #00CCCC;
+    width: 100px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+
+.box-card {
+  margin-bottom: 20px;
+}
 </style>
