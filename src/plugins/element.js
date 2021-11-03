807dd75b86f3ccbaaa3437851d03bf0ba6f120ec
fdlqiang <lq765952655@163.com>
2021-11-03 09:21:03 +0800

    aaa

---
+++ src/plugins/element.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Pagination,
+  Dialog,
+  InputNumber,
+  DatePicker,
+  Select,
+  Option,
+  Upload,
+  MessageBox,
+  Tooltip,
+  Tabs,
+  TabPane,
+  Link,
+  Slider
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(InputNumber)
+Vue.use(DatePicker)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Upload)
+Vue.use(Tooltip)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Link)
+Vue.use(Slider)
+Vue.prototype.$message = Message
+Vue.prototype.$messageBox = MessageBox
